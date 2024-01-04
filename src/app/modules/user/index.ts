b393b85eef6552316985809d372a5506873411d8
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2024-01-04 20:03:23 +0500

    converted index to ts

--- src/app/modules/user/index.ts
+++ src/app/modules/user/index.ts
@@ -3,19 +3,20 @@ const ERROR_TEXT = require("../../../utils/constants/errorText");
 const HTTP_STATUS = require("../../../utils/constants/httpStatus");
 const USER_ROLES = require("../../../utils/constants/userRoles");
 const Admin = require("./admin");
+const User = require("./user");
 const Manager = require("./manager");
 const Reader = require("./reader");
-const User = require("./user");
+import _ from "lodash";
 
 module.exports = {
-  getUser: async function (userId) {
+  getUser: async function (userId: number): Promise<typeof User> {
     const userDetails = await UserModel.findById(userId);
     if (!userDetails)
       throw Object({
         message: ERROR_TEXT.NO_USER,
         status: HTTP_STATUS.UNAUTHORIZED,
       });
-    let user = {};
+    let user: typeof User;
     switch (userDetails.role) {
       case USER_ROLES.ADMIN:
         user = new Admin(userDetails);
