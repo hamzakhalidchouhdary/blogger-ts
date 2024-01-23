058cc53e68cd3d07d6baaa0a91212c5d4ed15667
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2024-01-23 20:31:26 +0500

    converted index.js to index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,14 +1,15 @@
 require("dotenv").config();
 const cluster = require("cluster");
 const CPUCount = require("os").cpus().length;
-const express = require("express");
+import express from "express";
 const router = require("./routers/index");
 const { errorHandler } = require("./routers/v1/middleware/validations");
+import { Express } from "express";
 
 const PORT = process.env.PORT;
 const ENV = process.env.NODE_ENV;
 
-const app = express();
+const app: Express = express() as Express;
 
 app.use(express.json());
 app.use("/", router);
